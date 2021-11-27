5027606fb19e858ed9553cf2860995fdab102d8a
向山　翔 <hms0175km@gmail.com>
2021-11-27 20:01:40 +0900

    Nuxt.js practice7 画像を

---
+++ Nuxt.js_practice7/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="container">
+    <div>
+      <h1>hello</h1>
+      <img src="image/cat.jpg">
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+<style>
+  h1 {
+    color: green;
+  }
+</style>
